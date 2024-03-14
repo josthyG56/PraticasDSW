e9ef6f08b5341f8b61e7a13bac11b2c6c377ca8d
Fausto Ramirez <158236949+josthyG56@users.noreply.github.com>
2024-03-14 16:52:54 -0700

    Add files via upload

---
+++ practica 20/practica20.js
@@ -0,0 +1,12 @@
+var num1=5;
+var num2=8;
+
+if (num1<=num2) {
+    alert("el numero "+num1+" no es mayor que el numero "+num2);
+} if (num2<=0) {
+    alert("el numero "+num2+" es positvo.");
+} if (++num1<num2) {
+    alert("Incrementar uno en "+num1+", no lo hace mayor que numero "+num2);
+} else {
+    alert("Ninguna de las condiciones anteriores se cumplio.");    
+}
